715a5637c20efbf04b567ebd315e732c2acd4abf
Luka <LPolic@its.jnj.com>
2025-06-25 21:03:11 +0200

    Changing of annotation solved a problem.

--- cinema/src/main/java/com/example/cinema/controller/GlobalExceptionHandler.java
+++ cinema/src/main/java/com/example/cinema/controller/GlobalExceptionHandler.java
@@ -9,7 +9,9 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
